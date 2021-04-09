374228d23fbd0437293b38313b1721e20b03eb1a
AkankshaJjw <57306270+AkankshaJjw@users.noreply.github.com>
2021-04-09 11:19:48 -0400

    Create ltc2944_tester.ino

---
+++ ltc2944_tester/ltc2944_tester.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+
+void setup() {
+  
+  Wire.setSpeed(CLOCK_SPEED_100KHZ);
+  Wire.stretchClock(true);
+  Wire.begin();
+  LTC2944_mode = LTC2944_AUTOMATIC_MODE | prescalar_mode | alcc_mode ;
+  ack |= LTC2944_write(LTC2944_I2C_ADDRESS, LTC2944_CONTROL_REG, LTC2944_mode);
+
+}
+
+void loop() {
+
+  // Read 16-bit charge measurement
+  ack |= LTC2944_read_16_bits(LTC2944_I2C_ADDRESS, LTC2944_VOLTAGE_MSB_REG, &voltage_measurement);
+  // Convert voltage reading to Volts
+  batteryState.voltage_V = LTC2944_code_to_voltage(voltage_measurement);
+  Serial.print("Raw Battery Voltage: ");
+  Serial.println(voltage_measurement);
+
+}
